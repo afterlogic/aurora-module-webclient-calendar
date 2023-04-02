20a0357efd635c469ff19e5c90473d5df8e3f418
Vasil Sokolov <vasil@afterlogic.com>
2023-04-02 14:35:23 +0200

    Switched to Quasar2/VueJs3

--- vue/components/CalendarAdminSettings.vue
+++ vue/components/CalendarAdminSettings.vue
@@ -127,7 +127,7 @@ export default {
   },
 
   beforeRouteLeave (to, from, next) {
-    this.doBeforeRouteLeave(to, from, next)
+    this.$root.doBeforeRouteLeave(to, from, next)
   },
 
   methods: {
