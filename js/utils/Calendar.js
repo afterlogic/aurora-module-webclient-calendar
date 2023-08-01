d09bc9ee8d55bbf5ca1ea25994eadfc074d72391
Vasil Sokolov <vasil@afterlogic.com>
2023-08-01 14:32:11 +0200

    Moved CalendarUtils>getTimeListStepHour from CalendarWebClient to CoreWebclient

--- js/utils/Calendar.js
+++ js/utils/Calendar.js
@@ -3,46 +3,12 @@
 var
 	_ = require('underscore'),
 	$ = require('jquery'),
-	moment = require('moment'),
 
 	CalendarUtils = require('%PathToCoreWebclientModule%/js/utils/Calendar.js'),
 	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
 
 	Settings = require('modules/%ModuleName%/js/Settings.js')
 ;
-
-/**
- * Generates a list of time to display in calendar settings.
- * 
- * @param {string} sLabelFormat
- * @param {string} sValueFormat
- * @returns {Array}
- */
-CalendarUtils.getTimeListStepHour = function (sLabelFormat, sValueFormat)
-{
-	var 
-		aTimeList = [
-			'01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
-			'11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00',
-			'21:00', '22:00', '23:00', '00:00'
-		],
-		sLabelFormat = sLabelFormat || 'HH:mm'
-		sValueFormat = sValueFormat || 'H'
-	;
-	
-	return _.map(aTimeList, function (sTime) {
-		var
-			oMoment = moment(sTime, 'HH:mm'),
-			sText = oMoment.format(sLabelFormat),
-			sValue = oMoment.format(sValueFormat)
-		;
-		if (sTime === '00:00')
-		{
-			sValue = '24';
-		}
-		return {text: sText, value: sValue};
-	});
-};
 
 /**
  * @param {string} sSubject
