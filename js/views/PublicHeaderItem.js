abdbc05851e603c8aca5601acfdb8f0cf43d3978
Vasil Sokolov <vasil.sokolov@gmail.com>
2025-10-06 17:50:50 +0200

    fixed issue with delayed displaying calendar name by pub link

--- js/views/PublicHeaderItem.js
+++ js/views/PublicHeaderItem.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var
-	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-			
+	App = require('%PathToCoreWebclientModule%/js/App.js'),
+	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),			
 	CHeaderItemView = require('%PathToCoreWebclientModule%/js/views/CHeaderItemView.js'),
+
+	sTabTitle = App.isPublic() ? '' : TextUtils.i18n('%MODULENAME%/ACTION_SHOW_CALENDAR'),
 	
-	PublicHeaderItem = new CHeaderItemView(TextUtils.i18n('%MODULENAME%/ACTION_SHOW_CALENDAR'))
+	PublicHeaderItem = new CHeaderItemView(sTabTitle)
 ;
 
 module.exports = PublicHeaderItem;
